fd0e877f995d9420af1be6ad5aff35127ad13688
Torchin Anton <torchinae@yandex.ru>
2024-11-20 13:18:59 +0300

    add change number in start

--- pifagor.py
+++ pifagor.py
@@ -20,21 +20,22 @@ GREEN = (0, 128, 0)
 RED = (255, 0, 0)
 GREY = (128, 128, 128)
 # Коды цифр
-NUMBER_KEYBOARD = [1073741922, 1073741913, 1073741914, 1073741915, 1073741916,
-                   1073741917, 1073741918, 1073741919, 1073741920, 1073741921]
-NUMBER_KEYBOARD_HI = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
+TextSting_KEYBOARD = [1073741922, 1073741913, 1073741914, 1073741915,
+                      1073741916, 1073741917, 1073741918, 1073741919,
+                      1073741920, 1073741921]
+TextSting_KEYBOARD_HI = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
 # не играемые цифры
-set_stop_numbers = [3, 4, 5, 6, 7, 8, 9]
+set_stop_TextStings = []
 
 
-class Number():
+class TextSting():
     def __init__(self,
                  value=0,
                  coordinate=(SCREEN_WIDTH/3-10, SCREEN_HEIGHT/2-HEIGH_NUM/2),
                  color=TEXT_COLOR):
         self.value = value
         self.coordinate = coordinate
-        self.height_number = HEIGH_NUM
+        self.height_TextSting = HEIGH_NUM
         self.color = color
 
     def delta(self, x=0, y=0):
@@ -43,7 +44,7 @@ class Number():
     def draw(self, screen):
         """Вывод текста на экран."""
         pygame.font.init()
-        font = pygame.font.Font(None, self.height_number)
+        font = pygame.font.Font(None, self.height_TextSting)
         text = font.render(str(self.value), True, self.color)
         text_rect = text.get_rect(topleft=(self.coordinate))
         screen.blit(text, text_rect)
@@ -62,55 +63,70 @@ class Button():
         self.width = width
         self.height = height
         self.incorrect_answer = defaultdict(int)
+        self.rect = pygame.Rect(coordinats[0], coordinats[1], width, height)
+        self.block = False
 
     def __str__(self) -> str:
         return str(self.name) + ' - ' + ', '.join(str(x) for x in self.flag)
 
+    @staticmethod
+    def write_text(text_input, button_rect, screen):
+        # Определяем шрифт и текст кнопки
+        font = pygame.font.Font(None, 36)
+        text = font.render(text_input, True, TEXT_COLOR)
+        text_rect = text.get_rect(center=button_rect.center)
+        screen.blit(text, text_rect)
+
     def draw_point(self, screen):
-        global set_stop_numbers
-        for i, point in enumerate(self.flag):
+        for i in range(len(self.flag)):
             circle_center = (self.coordinats[0] + self.width // 2,
                              self.coordinats[1] - 10 * i-10)
             circle_radius = 3
-            if point is True:
+            if self.flag[i] is True:
                 color = GREEN
             else:
                 color = RED
-            if int(self.name) in set_stop_numbers:
+            if self.name in set_stop_TextStings:
                 color = GREY
-            pygame.draw.circle(screen, color, circle_center, circle_radius, 0)
+            pygame.draw.circle(screen, color,
+                               circle_center, circle_radius, 0)
 
     def draw(self, screen):
         "Отрисовка кнопки."
-        global set_stop_numbers
-        if int(self.name) in set_stop_numbers:
-            self.color = GREY
-        button_rect = pygame.Rect(self.coordinats[0], self.coordinats[1],
-                                  self.width, self.height)
-        pygame.draw.rect(screen, self.color, button_rect)
-        # Определяем шрифт и текст кнопки
-        font = pygame.font.Font(None, 36)
-        text = font.render(self.name, True, TEXT_COLOR)
-        text_rect = text.get_rect(center=button_rect.center)
-        screen.blit(text, text_rect)
-        self.draw_point(screen)
+        global set_stop_TextStings
+        pygame.draw.rect(screen, self.color, self.rect)
+        self.write_text(self.name, self.rect, screen)
 
 
-def handle_keys() -> None:
+def handle_keys(buttons_list: list, screen) -> str:
     """Функция обработки действий пользователя."""
-    pressed_digit = None
+    global set_stop_TextStings
+    pressed_digit = ''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
-            """ if button_rect.collidepoint(mouse_pos):
-                print("Кнопка была нажата!")"""
+            for but in buttons_list:
+                if but.block is False and but.rect.collidepoint(mouse_pos):
+                    print("Кнопка была нажата!", but.name)
+                    if but.name != 'СТАРТ!' and but.color == GREEN:
+                        but.color = GREY
+                        set_stop_TextStings.append(int(but.name))
+                    elif but.name != 'СТАРТ!' and but.color == GREY:
+                        but.color = GREEN
+                        set_stop_TextStings.remove(int(but.name))
+                    if but.name == 'СТАРТ!':
+                        print("Кнопка была нажата выхода", but.name)
+                        return but.name
+                    but.draw(screen)
+                    pygame.display.update(but.rect)
+                    mouse_pos = (0, 0)
         elif event.type == pygame.KEYDOWN:
-            if event.key in (NUMBER_KEYBOARD_HI):
+            if event.key in (TextSting_KEYBOARD_HI):
                 pressed_digit = pygame.key.name(event.key)
-            elif event.key in NUMBER_KEYBOARD:
+            elif event.key in TextSting_KEYBOARD:
                 pressed_digit = pygame.key.name(event.key)[1]
             elif pygame.key.name(event.key) == 'backspace':
                 pressed_digit = 'backspace'
@@ -127,27 +143,29 @@ def display_clear(screen) -> None:
     pygame.display.update()
 
 
-def display_write(screen, arr_num: list):
+def display_write(screen, arr_num: list) -> None:
     "Отрисовка вопроса"
     display_clear(screen)
     for x in arr_num:
         x.draw(screen)
 
 
-def create_buttons(buttons_list):
+def create_buttons(buttons_list: list) -> None:
     for x in range(2, 10):
         btn = Button(name=str(x), coordinats=(x*60-30, SCREEN_HEIGHT - 80))
         buttons_list.append(btn)
 
 
-def draw_buttons(buttons_list, screen):
+def draw_buttons(buttons_list: list, screen) -> None:
     "Отрисовка кнопок цифр и пройденных заданий"
     for btn in buttons_list:
         btn.draw(screen)
+        if btn.name not in set_stop_TextStings and btn.name != 'СТАРТ!':
+            btn.draw_point(screen)
     pygame.display.update()
 
 
-def save_answer(buttons_list, answer, correct_flag):
+def save_answer(buttons_list, answer, correct_flag: bool) -> None:
     num = answer[0]-2
     if correct_flag:
         buttons_list[num].flag[answer[1]-2] = correct_flag
@@ -156,12 +174,32 @@ def save_answer(buttons_list, answer, correct_flag):
         buttons_list[num].incorrect_answer[answer[1]] += 1
         print(f'ошибка {answer[0]} +1', buttons_list[num].incorrect_answer)
 
-def start_menu(screen):
-    
+
+def start_menu(buttons_list, screen):
+    text = TextSting('Выбери цифры', (SCREEN_WIDTH//6, SCREEN_HEIGHT//4 - 50))
+    text.draw(screen)
+    btn = Button('СТАРТ!',
+                 (SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 25),
+                 False, RED,
+                 100, 50)
+    buttons_list.append(btn)
+    draw_buttons(buttons_list, screen)
+    pygame.display.update()
+    while True:
+        if handle_keys(buttons_list, screen) == 'СТАРТ!':
+            print('buttons_list', [x.name for x in buttons_list])
+            buttons_list.pop(-1)
+            print('buttons_list 2', [x.name for x in buttons_list])
+            for btn in buttons_list:
+                btn.block = True
+            display_clear(screen)
+            return
+
 
 def finish(screen):
-    text1 = Number('Молодец!', (SCREEN_WIDTH//4 + 15, SCREEN_HEIGHT//2 - 50))
-    text2 = Number('Игра закончена.', (SCREEN_WIDTH//6, SCREEN_HEIGHT//2))
+    text1 = TextSting(
+        'Молодец!', (SCREEN_WIDTH//4 + 15, SCREEN_HEIGHT//2 - 50))
+    text2 = TextSting('Игра закончена.', (SCREEN_WIDTH//6, SCREEN_HEIGHT//2))
     text1.draw(screen)
     text2.draw(screen)
     pygame.display.update()
@@ -172,36 +210,42 @@ def finish(screen):
 
 def main():
     # Настройка игрового окна:
+    global set_stop_TextStings
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
     pygame.display.set_caption('Пифагоровы штаны')
     buttons_list = []
     create_buttons(buttons_list)
-    set_of_numbers = [x for x in range(2, 10) if x not in set_stop_numbers]
-    combinations_numbers = list(product(set_of_numbers, range(2, 9)))
-    shuffle(combinations_numbers)
-    pop_num = combinations_numbers.pop(0)
-    num_1 = Number(pop_num[0])
-    sign_multiply = Number('x')
-    num_2 = Number(pop_num[1])
-    sign_equal = Number('=')
-    answer = Number('?')
-    count_question = Number(f'Осталось примеров: {len(combinations_numbers)}',
-                            (SCREEN_WIDTH - 260, 10))
-    count_question.height_number = 30
+    start_menu(buttons_list, screen)
+    set_of_TextStings = [x for x in range(2, 10)
+                         if x not in set_stop_TextStings]
+    print('set_of_TextStings', set_of_TextStings)
+    combinations_TextStings = list(product(set_of_TextStings, range(2, 10)))
+    print(combinations_TextStings)
+    shuffle(combinations_TextStings)
+    pop_num = combinations_TextStings.pop(0)
+    num_1 = TextSting(pop_num[0])
+    sign_multiply = TextSting('x')
+    num_2 = TextSting(pop_num[1])
+    sign_equal = TextSting('=')
+    answer = TextSting('?')
+    count_comb = len(combinations_TextStings)
+    count_question = TextSting(f'Осталось примеров: {count_comb}',
+                               (SCREEN_WIDTH - 260, 10))
+    count_question.height_TextSting = 30
     sign_multiply.delta(40)
     num_2.delta(80)
     sign_equal.delta(120)
     answer.delta(160)
-    check_text = Number('Правильно', color=(255, 0, 0))
-    check_text.delta(-40, - check_text.height_number - 10)
-    check_text_answer = Number('')
+    check_text = TextSting('Правильно', color=(255, 0, 0))
+    check_text.delta(-40, - check_text.height_TextSting - 10)
+    check_text_answer = TextSting('')
     task = [num_1, sign_multiply, num_2, sign_equal, answer, count_question]
     display_write(screen, task)
     draw_buttons(buttons_list, screen)
     start_time = time.time()
     while True:
-        h_k = handle_keys()
+        h_k = handle_keys(buttons_list, screen)
         if h_k == 'enter' and answer.value.isdigit():
             if int(num_1.value) * int(num_2.value) == int(answer.value):
                 check_text.value = 'Правильно!'
@@ -209,13 +253,13 @@ def main():
                 time_delay = 1000
                 print(time.time() - start_time)
                 if time.time() - start_time > TIME_LIMIT_ANSWER:
-                    combinations_numbers.append(pop_num)
+                    combinations_TextStings.append(pop_num)
                     print('append')
                 save_answer(buttons_list, (num_1.value, num_2.value), True)
             else:
                 check_text.value = 'НЕ Правильно!'
                 check_text.color = RED
-                combinations_numbers.append(pop_num)
+                combinations_TextStings.append(pop_num)
                 print('append')
                 save_answer(buttons_list, (num_1.value, num_2.value), False)
                 time_delay = 2500
@@ -228,13 +272,13 @@ def main():
             pygame.time.delay(time_delay)
             display_clear(screen)
             # Новый вопрос.
-            if len(combinations_numbers) == 0:
+            if len(combinations_TextStings) == 0:
                 finish(screen)
             check_text_answer.value = ''
             count_question.value = 'Осталось примеров: '
-            count_question.value += str(len(combinations_numbers))
+            count_question.value += str(len(combinations_TextStings))
             if check_text.value == 'Правильно!':
-                pop_num = combinations_numbers.pop(0)
+                pop_num = combinations_TextStings.pop(0)
                 num_1.value = pop_num[0]
                 num_2.value = pop_num[1]
             answer.value = '?'
